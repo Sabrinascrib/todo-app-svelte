337a115b70681f51d585b31d86e7c824fcf12061
sabrina <sabrinaabrodi20@gmail.com>
2022-11-06 20:35:24 +0100

    Feat: creating header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { items } from "../stores";
+  $: itemCount = $items.length;
+  $: completedItemCount = $items.filter((item) => item.complete).length;
+</script>
+
+<style>
+    .header {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 15px 20px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 25px;
+    background: rgba(0, 0, 0, 0.1);
+  }
+
+  span {
+ font-size: 15px;
+  }
+</style>
+
+<div class="header">
+  <div>The To-Do List</div>
+  <span> {completedItemCount}/{itemCount} </span>
+</div>
