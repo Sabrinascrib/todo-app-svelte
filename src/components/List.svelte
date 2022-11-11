ede59563b5059ab2cacb7c2baf790e84f245b187
sabrina <sabrinaabrodi20@gmail.com>
2022-11-11 20:29:33 +0100

    feat: getting items

---
+++ src/components/List.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from "svelte";
+  import { items } from "../stores";
+  import TodoApi from "../ToDoApi"
+
+
+
+  onMount(async () => {
+    $items = await TodoApi.getAll();
+  });
+</script>
