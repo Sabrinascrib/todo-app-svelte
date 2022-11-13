80769f0d43d328f3369a71a37a143cfa91679263
sabrina <sabrinaabrodi20@gmail.com>
2022-11-13 19:35:57 +0100

    Feat: adding text input and checkbox

--- src/components/Items.svelte
+++ src/components/Items.svelte
@@ -1,3 +1,8 @@
 <script>
-    
+    export let id, text, completed;
 </script>
+
+
+
+<input class="text-input" type="text" bind:value={text} readonly={completed}>
+<input class="completed-checkbox" type="checkbox" bind:checked={completed}>
