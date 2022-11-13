b741eb3816c9cfb0a18eb40eeb5d5dfef9e086e2
sabrina <sabrinaabrodi20@gmail.com>
2022-11-13 19:35:06 +0100

    sample

--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,15 @@
 import { writable } from 'svelte/store'
 
 // Stores each To-Do list item
-export const items = writable([])
+export const items = writable([
+  {
+    id: 1,
+    text: 'hello',
+    completed: false,
+  },
+  {
+    id: 2,
+    text: 'hel343lo',
+    completed: true,
+  },
+])
