6b5bf81c899b8e27b0700eb6238781a6e1fb6d8d
sabrina <sabrinaabrodi20@gmail.com>
2022-11-13 19:36:54 +0100

    Feat: each block items

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -2,10 +2,33 @@
   import { onMount } from "svelte";
   import { items } from "../stores";
   import TodoApi from "../ToDoApi"
-
+  import Item from "./Items.svelte";
 
 
   onMount(async () => {
     $items = await TodoApi.getAll();
   });
 </script>
+
+<style> 
+  .list {
+    padding: 15px;
+  }
+
+  .list-status {
+    margin: 0;
+    text-align: center;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 1.1em;
+  }
+</style>
+
+<div class="list">
+
+  {#each $items as item (item)}
+   <Item {...item} />
+  {:else}
+    <p class="list-status">No Items Exist</p>
+  {/each}
+</div>
