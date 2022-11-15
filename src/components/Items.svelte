2ebd147775e17402570d43033fa7d7aa5a9edbdb
sabrina <sabrinaabrodi20@gmail.com>
2022-11-15 19:46:20 +0100

    fix: adding item div

--- src/components/Items.svelte
+++ src/components/Items.svelte
@@ -37,5 +37,7 @@
   }
 </style>
 
+<div class="item">
 <input class="text-input" type="text" bind:value={text} readonly={completed}>
 <input class="completed-checkbox" type="checkbox" bind:checked={completed}>
+</div>
